06c157d8783735a402ed2f38f4df88412ccdf996
Bdkelly <benjaminkelly97@gmail.com>
2025-12-06 11:12:16 -0500

    rew

--- RLAgent/CriticNet.py
+++ RLAgent/CriticNet.py
@@ -2,17 +2,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Critic(nn.Module):
-    def __init__(self, state_size, action_size):
-        super(Critic, self).__init__()
-        self.fcs1 = nn.Linear(state_size, 256)
-        self.fc2 = nn.Linear(256 + 1, 128) 
-        self.fc3 = nn.Linear(128, 1)
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / (fan_in ** 0.5)
+    return (-lim, lim)
 
-    def forward(self, state, action):
-        xs = F.relu(self.fcs1(state))
-        x = torch.cat([xs, action], 1)
+class Critic(nn.Module):
+    def __init__(self, state_size, action_size, max_action, fc1_units=256, fc2_units=128):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.ln1 = nn.LayerNorm(fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.ln2 = nn.LayerNorm(fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
         
-        x = F.relu(self.fc2(x))
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+    def forward(self, state, action):
+        xs = self.fcs1(state)
+        xs = self.ln1(xs)
+        xs = F.relu(xs)
+        
+        x = torch.cat((xs, action), dim=1)
+        x = self.fc2(x)
+        x = self.ln2(x)
+        x = F.relu(x) 
+        
         q_value = self.fc3(x)
         return q_value
